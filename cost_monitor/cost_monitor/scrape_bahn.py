db15a1677ee251eeed721741349ffecb1a3dba1b
Christian Bartz <christian.bartz@hpi.de>
2022-06-06 13:54:08 +0000

    also extract fare information from bahn page, save info in model classes

--- cost_monitor/cost_monitor/scrape_bahn.py
+++ cost_monitor/cost_monitor/scrape_bahn.py
@@ -1,17 +1,23 @@
 import re
+import datetime
 import time
-from tracemalloc import start
 
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import Playwright
+
+from cost_monitor.models import Connection, Journey, Fare
 
 
 TIME_RE = re.compile('(?P<time>\d\d:\d\d)')
+FARE_RE = re.compile('(?P<fare>\d+,\d\d).*€')
 TIME_PATTERN = "%H:%M"
 
-def extract_times(results_container):
+def extract_times(results_container, journey: Journey):
     connections = results_container.query_selector_all(".overviewConnection")
 
-    last_start_time = time.strptime("00:00", TIME_PATTERN)
+    last_start_time = datetime.time(*time.strptime("00:00", TIME_PATTERN)[3:6])
+
+    extracted_connections = []
+    fares = []
 
     for connection in connections:
         connection_times = connection.query_selector_all(".connectionTime")
@@ -20,16 +26,24 @@ def extract_times(results_container):
         start_time = TIME_RE.search(connection_time.query_selector(".timeDep").inner_text()).group('time')
         end_time = TIME_RE.search(connection_time.query_selector(".timeArr").inner_text()).group('time')
 
-        found_start_time = time.strptime(start_time, TIME_PATTERN)
+        found_start_time = datetime.time(*time.strptime(start_time, TIME_PATTERN)[3:6])
+        found_end_time = datetime.time(*time.strptime(end_time, TIME_PATTERN)[3:6])
         if found_start_time > last_start_time:
             last_start_time = found_start_time
+
+        extracted_connections.append(Connection(journey=journey, start_time=found_start_time, end_time=found_end_time))
+
+        fare_output = connection.query_selector(".fareOutput")
+        fare = FARE_RE.search(fare_output.inner_text()).group('fare')
+        fare = float(fare.replace(",", "."))
+        fares.append(fare)
 
         print(start_time)
         print(end_time)
 
-    return last_start_time
+    return last_start_time, extracted_connections, fares
 
-def run(playwright: Playwright) -> None:
+def extract_fares(playwright: Playwright, journey: Journey) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
 
@@ -49,23 +63,24 @@ def run(playwright: Playwright) -> None:
     page.locator("input[name=\"S\"]").click()
 
     # Fill input[name="S"]
-    page.locator("input[name=\"S\"]").fill("München Hbf")
+    page.locator("input[name=\"S\"]").fill(journey.start)
 
     # Press Tab
     page.locator("input[name=\"S\"]").press("Tab")
 
     # Fill input[name="Z"]
-    page.locator("input[name=\"Z\"]").fill("Berlin hbf")
+    page.locator("input[name=\"Z\"]").fill(journey.end)
 
     date_input = page.locator("input[name=\"date\"]")
     date_input.evaluate("(node) => node.removeAttribute('readonly')")
-    date_input.fill("Do, 14.07.2022")
+    # date_input.fill("Do, 14.07.2022")
+    date_input.fill(journey.day.strftime("%a, %d.%m.%Y"))
 
     # Click input[name="time"]
     page.locator("input[name=\"time\"]").click()
 
     # Fill input[name="time"]
-    page.locator("input[name=\"time\"]").fill("13:00")
+    page.locator("input[name=\"time\"]").fill(journey.earliest_start_time.strftime("%H:%M"))
 
     # Click input:has-text("Suchen")
     # with page.expect_navigation(url="https://reiseauskunft.bahn.de/bin/query.exe/dn?revia=yes&existOptimizePrice-deactivated=1&country=DEU&dbkanal_007=L01_S01_D001_qf-bahn-svb-kl2_lz03&start=1&protocol=https%3A&S=M%C3%BCnchen+Hbf&REQ0JourneyStopsSID=&Z=Berlin+hbf&REQ0JourneyStopsZID=&date=Do%2C+14.07.2022&time=13%3A00&timesel=depart&returnDate=&returnTime=&returnTimesel=depart&optimize=0&auskunft_travelers_number=1&tariffTravellerType.1=E&tariffTravellerReductionClass.1=0&tariffClass=2&rtMode=DB-HYBRID&externRequest=yes&HWAI=JS%21js%3Dyes%21ajax%3Dyes%21&externRequest=yes&HWAI=JS%21js%3Dyes%21ajax%3Dyes%21#hfsseq1|mp.02695317.1652630071"):
@@ -76,9 +91,11 @@ def run(playwright: Playwright) -> None:
     # ---------------------
 
     results = page.wait_for_selector("#resultsOverviewContainer")
+    connections = None
+    fares = None
     while True:
-        last_start_time = extract_times(results)
-        if last_start_time > time.strptime("18:00", TIME_PATTERN):
+        last_start_time, connections, fares = extract_times(results, journey)
+        if last_start_time > journey.latest_start_time:
             break
 
         later_button = results.query_selector(".later")
@@ -86,10 +103,8 @@ def run(playwright: Playwright) -> None:
             later_button.click()
         results = page.wait_for_selector("#resultsOverviewContainer")
         print("new round")
-
+                        
     context.close()
     browser.close()
 
-
-with sync_playwright() as playwright:
-    run(playwright)
+    return connections, fares
